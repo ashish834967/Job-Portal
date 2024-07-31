ce454ec87ab8d4a7e544c13ca989886565663440
ashish834967 <ashishpatidar011@gmail.com>
2024-07-31 09:58:34 +0530

    modified isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
+      //  console.log(token)
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
@@ -21,6 +22,10 @@ const isAuthenticated = async (req, res, next) => {
         next();
     } catch (error) {
         console.log(error)
+         return res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        });
     }
 }
 
